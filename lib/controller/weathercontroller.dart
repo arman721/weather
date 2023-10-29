20213fe57a1e8eadc5a40d77a026fecdc222ff10
Arman <119151658+arman721@users.noreply.github.com>
2023-10-29 10:39:16 +0530

    third commit

--- lib/controller/weathercontroller.dart
+++ lib/controller/weathercontroller.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class WeatherController extends GetxController {
   RxInt dayindex = 0.obs;
@@ -16,15 +16,14 @@ class WeatherController extends GetxController {
 
   Future loaddata() async {
     try {
-      // var response = await http.get(Uri.parse(
-      //     "http://api.weatherapi.com/v1/forecast.json?key=61543a2a551c42df8b094726232810&q=Cuttack&days=5&aqi=no&alerts=no"));
-      // final decodedlist = jsonDecode(response.body);
       await Future.delayed(Duration(seconds: 2));
-      var weatherdataJson =
-          await rootBundle.loadString("lib/models/weatherdata.json");
-      final decoded = jsonDecode(weatherdataJson);
-      Get.log("${decoded["forecast"]["forecastday"][0]["date"]}");
-      weatherdata.value = decoded;
+      var response = await http.get(Uri.parse(
+          "http://api.weatherapi.com/v1/forecast.json?key=61543a2a551c42df8b094726232810&q=Cuttack&days=5&aqi=no&alerts=no"));
+      final decodedlist = jsonDecode(response.body);
+      // var weatherdataJson =
+      //     await rootBundle.loadString("lib/models/weatherdata.json");
+      // final decoded = jsonDecode(weatherdataJson);
+      weatherdata.value = decodedlist;
       // Get.log("$weatherdata]");
     } on Exception catch (e) {
       Get.log(" the error is $e");
